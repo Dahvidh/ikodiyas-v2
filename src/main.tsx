93ae6db7882b2bb4e2119c41228ac2c174fae037
David Nnamdi <95830108+Dahvidh@users.noreply.github.com>
2026-09-10 00:12:55 +0100

    Import global CSS styles in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import AppRouter from "./router/AppRouter";
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
